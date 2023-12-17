46238f26a7ff6fc587a6db3b14cbbd9fe78f12ad
Shibin S P <shibinsp45@gmail.com>
2023-12-17 16:32:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState(0);
+
+  const handleClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input));
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setResult(0);
+  };
+
+  return (
+    <div className="calculator">
+      <h1 style={{textAlign:'center'}}>Basic Calculator </h1>
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('0')}>0</button>
+
+        <button onClick={() => handleClick('/')}>/</button>
+        <button onClick={handleClear}>C</button>
+        <button onClick={handleCalculate}>=</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
